ec4e06aa8c827955e2d0c472c4bc50c069e5eb75
Emeric GOUY <emeric.gouy@ens.uvsq.fr>
2020-09-23 16:35:09 +0200

    test toString

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello");
+        Fraction test = new Fraction(2,4);
+        System.out.println(test.toString());
     }
 }
